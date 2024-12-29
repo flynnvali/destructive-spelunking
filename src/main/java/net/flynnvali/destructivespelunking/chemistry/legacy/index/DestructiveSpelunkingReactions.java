657954fcd267cb2853dffd14b030957150f8213b
flynnvali <flynnherr@gmail.com>
2024-12-29 16:00:18 -0600

    initial

--- src/main/java/net/flynnvali/destructivespelunking/chemistry/legacy/index/DestructiveSpelunkingReactions.java
+++ src/main/java/net/flynnvali/destructivespelunking/chemistry/legacy/index/DestructiveSpelunkingReactions.java
@@ -6,6 +6,7 @@ import com.petrolpark.destroy.block.DestroyBlocks;
 import com.petrolpark.destroy.chemistry.legacy.LegacyReaction;
 import com.petrolpark.destroy.chemistry.legacy.LegacyReaction.ReactionBuilder;
 import com.petrolpark.destroy.chemistry.legacy.index.DestroyMolecules;
+import com.petrolpark.destroy.chemistry.legacy.index.DestroyReactions;
 import com.petrolpark.destroy.chemistry.legacy.reactionresult.CombinedReactionResult;
 import com.petrolpark.destroy.chemistry.legacy.reactionresult.ExplosionReactionResult;
 import com.petrolpark.destroy.chemistry.legacy.reactionresult.PrecipitateReactionResult;
@@ -13,21 +14,21 @@ import com.petrolpark.destroy.item.DestroyItems;
 import com.petrolpark.destroy.config.DestroySubstancesConfigs;
 import com.simibubi.create.AllItems;
 import com.simibubi.create.AllTags;
-import com.petrolpark.destroy.chemistry.legacy.index.DestroyReactions;
+// import com.petrolpark.destroy.chemistry.legacy.index.DestroyReactions;
 
 import net.minecraft.world.item.Items;
 
-public class DestroyReactions {
+public class DestructiveSpelunkingReactions extends DestroyReactions {
     public static final LegacyReaction
 
-    SALTPETER_DISSOLUTION = builder()
-    .id("saltpeter_dissolution")
-    .addSimpleItemTagReactant(AllTags.forgeItemTag("dusts/zinc"), 3f)
-    .addCatalyst(DestroyMolecules.WATER, 0)
-    .addProduct(DestroyMolecules.POTASSIUM_ION, 5)
-    .addProduct(DestroyMolecules.NITRATE, 5)
-     .build();
-static{};
+            SALTPETER_DISSOLUTION = builder()
+            .id("saltpeter_dissolution")
+            .addSimpleItemTagReactant(AllTags.forgeItemTag("dusts/saltpeter"), 3f)
+            .addCatalyst(DestroyMolecules.WATER, 0)
+            .addProduct(DestroyMolecules.POTASSIUM_ION, 5)
+            .addProduct(DestroyMolecules.NITRATE, 5)
+            .build();
+    static{};
 
 
 
